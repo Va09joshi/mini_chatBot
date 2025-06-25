7bf6a35900e3859d5b60fc5e712e20e96c0412e5
Va09joshi <122175840+Va09joshi@users.noreply.github.com>
2025-06-25 18:46:48 +0530

    Update api_constants.dart

--- lib/constant/api_constants.dart
+++ lib/constant/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiContant {
-  static const String apiKey = "AIzaSyCDTqT49V9wzQ-MCg_B0nuLV_2usVKhemM";
+  static const String apiKey = "Your Api key";
   static const String baseUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
 }
